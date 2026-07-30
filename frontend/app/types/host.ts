5d714dc6f5290bfc22276825000e0c6aa46d4d87
JoelVCrasta <loejstarc@gmail.com>
2026-07-30 15:53:37 +0530

    feat: added host type

--- frontend/app/types/host.ts
+++ frontend/app/types/host.ts
@@ -1,8 +1,17 @@
 export interface HostDeviceResponse {
-  id: string
+  id: number
   host_id: string
   org_id: string
   name: string
   is_online: boolean
   last_seen: string
+}
+
+export interface HostDevice {
+  id: number
+  hostId: string
+  orgId: string
+  name: string
+  isOnline: boolean
+  lastSeen: string
 }
