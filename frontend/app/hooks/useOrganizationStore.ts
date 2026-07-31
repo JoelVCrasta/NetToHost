c0958d9e44ba1eb1d6b8c6dec3fe5f833583da1e
JoelVCrasta <loejstarc@gmail.com>
2026-07-31 16:35:27 +0530

    refactor: removed all orgs

--- frontend/app/hooks/useOrganizationStore.ts
+++ frontend/app/hooks/useOrganizationStore.ts
@@ -4,9 +4,7 @@ import type { Organization } from "~/types/organization"
 
 interface OrganizationState {
   selectedOrg: Organization | null
-  allOrganizations: Organization[]
   setSelectedOrg: (org: Organization | null) => void
-  setAllOrganizations: (orgs: Organization[]) => void
   clearOrganizationStore: () => void
 }
 
@@ -14,15 +12,11 @@ export const useOrganizationStore = create<OrganizationState>()(
   persist(
     (set) => ({
       selectedOrg: null,
-      allOrganizations: [],
       setSelectedOrg: (org: Organization | null) => {
         set({ selectedOrg: org })
       },
       clearOrganizationStore: () => {
         set({ selectedOrg: null })
-      },
-      setAllOrganizations: (orgs: Organization[]) => {
-        set({ allOrganizations: orgs })
       },
     }),
     {
